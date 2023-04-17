a07ef0d1ec9fb46bf4535e8b3bc3c077d1f1404b
Pitchapa <6331305019@lamduan.mfu.ac.th>
2023-04-17 14:45:08 +0700

    report

--- pages/report.vue
+++ pages/report.vue
@@ -1,22 +1,44 @@
 <template>
-  <v-row justify="center" align="baseline">
-    <v-col cols="36" md="24" sm="12" xl="36"> </v-col>
-
-    <v-col cols="36" md="36" sm="12">
-      <v-btn
-        color="primary"
-        depressed
-        elevation="36"
-        large
-        x-large
-        nuxt
-        to="/historybuyapart"
-      >
-        การแจ้งเตือน
-      </v-btn>
-    </v-col>
-  </v-row>
+  <v-col cols="12" sm="10" md="4">
+    <v-text-field
+      label="Filled"
+      placeholder="Dense & Rounded"
+      filled
+      rounded
+      dense
+    >
+    </v-text-field>
+    <v-btn elevation="2"> ค้นหา</v-btn>
+    <v-btn elevation="2"> เพิ่มพนักงาน</v-btn>
+    <v-data-table
+      :headers="headers"
+      :items="theemployee"
+      :items-per-page="5"
+      class="elevation-2"
+      density="compact"
+      item-key="name"
+    >
+    </v-data-table>
+  </v-col>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "No.",
+          align: "numberemploy",
+          sortable: false,
+          value: "name",
+        },
+        { text: "หมายเลขพนักงาน", value: "calories" },
+        { text: "ชื่อ-นามสกุล", value: "nameemployee" },
+        { text: "เลือก", value: "chooseemploy" },
+      ],
+      theemployee: [],
+    };
+  },
+};
 </script>
